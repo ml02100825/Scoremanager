1785fdc919b1b879a8cfd8af505489c8016c407f
oyu1731 <ktq2390303@o-hara.ac.jp>
2024-04-30 12:21:31 +0900

    ログイン機能修正

--- src/scoremanager/LoginExecuteAction.java
+++ src/scoremanager/LoginExecuteAction.java
@@ -11,58 +11,50 @@ import bean.Teacher;
 import dao.TeacherDAO;
 import tool.Action;
 
-
 public class LoginExecuteAction extends Action {
 
-	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		//ローカル変数の宣言 1
-		String url = "";
-		String id = "";
-		String password = "";
-		TeacherDAO teacherDAO = new TeacherDAO();
-		Teacher teacher = null;
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // ローカル変数の宣言
+        String url = "";
+        String id = "";
+        String password = "";
+        TeacherDAO teacherDAO = new TeacherDAO();
+        Teacher teacher = null;
 
-		//リクエストパラメータ―の取得 2
-		id = request.getParameter("id");// 教員ID
-		password = request.getParameter("password");//パスワード
+        // リクエストパラメータ―の取得
+        id = request.getParameter("id"); // 教員ID
+        password = request.getParameter("password"); // パスワード
 
-		//DBからデータ取得 3
-		teacher = teacherDAO.login(id, password);//教員データ取得
-		System.out.println(teacher);
+        // DBからデータ取得
+        teacher = teacherDAO.login(id, password); // 教員データ取得
 
-		//ビジネスロジック 4
-		//DBへデータ保存 5
-		//レスポンス値をセット 6
-		//フォワード 7
-		//条件で手順4~7の内容が分岐
-		if (teacher != null) {// 認証成功の場合
-			// セッション情報を取得
-			HttpSession session = request.getSession(true);
-			// 認証済みフラグを立てる
-			teacher.setAuthenticated(true);
-			// セッションにログイン情報を保存
-			session.setAttribute("user", teacher);
+        // ビジネスロジック
+        if (teacher != null) { // 認証成功の場合
+            // セッション情報を取得
+            HttpSession session = request.getSession(true);
+            // 認証済みフラグを立てる
+            teacher.setAuthenticated(true);
+            // セッションにログイン情報を保存
+            session.setAttribute("user", teacher);
 
-			//リダイレクト
-			url = "main/Menu.action";
-			response.sendRedirect(url);
-		} else {
-			// 認証失敗の場合
-			// エラーメッセージをセット
-			List<String> errors = new ArrayList<>();
-			errors.add("IDまたはパスワードが確認できませんでした");
-			request.setAttribute("errors", errors);
-			// 入力された教員IDをセット
-			request.setAttribute("id", id);
-			System.out.print(errors);
+            // リダイレクト
+            url = "main/Menu.action";
+            response.sendRedirect(url);
+        } else {
+            // 認証失敗の場合
+            // エラーメッセージをセット
+            List<String> errors = new ArrayList<>();
+            errors.add("ログインに失敗しました。IDまたはパスワードが正しくありません。");
+            request.setAttribute("errors", errors);
+            // エラーメッセージの色をセット
+            request.setAttribute("errorColor", "color: black;");
+            // 入力された教員IDをセット
+            request.setAttribute("id", id);
 
-			//フォワード
-			url = "login.jsp";
-			request.getRequestDispatcher(url).forward(request, response);
-		}
-
-//		req.getRequestDispatcher(url).forward(req, res);
-	}
-
+            // フォワード
+            url = "login.jsp";
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+    }
 }
